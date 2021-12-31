b215252ac0affb1d7ace1341524ab961fd717bde
Vladimir Timofeev <vovkasm@gmail.com>
2021-12-31 11:30:56 +0300

    allow typechecking from `npm test`, remove old tests

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import {Navigation} from 'react-native-navigation';
 
 import App from './App';
+import AnimatedTwoColumns from './AnimatedTwoColumns';
 
 Navigation.registerComponent('App', () => App);
+Navigation.registerComponent('AnimatedTwoColumns', () => AnimatedTwoColumns);
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
       stack: {
-        children: [{component: {name: 'App'}}],
+        id: 'Main',
+        children: [
+          {
+            component: {
+              name: 'App',
+              options: {topBar: {title: {text: 'RN Demo'}}},
+            },
+          },
+        ],
       },
     },
   });
